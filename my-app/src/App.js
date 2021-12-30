a6f6bf0bf8cc670b8784a5df6644edfd44469097
JalenMcC <76828457+JalenMcC@users.noreply.github.com>
2021-12-29 20:27:46 -0500

    Added NavigationBar Component (WIP)
    Added a component to serve as a navigation bar for the website. Still has some kinks to work out at the moment

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import NavigationBar from './NavigationBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <NavigationBar />
     </div>
   );
 }
---
+++ my-app/src/NavigationBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function NavigationBar() {
+    return(
+        <div>
+            <header>
+                <nav>
+                    <ul class = "bar">
+                        <li><a href="" >About Me</a></li>
+                        <li><a href="" >Current Objectives</a></li>
+                        <li><a href="" >Free Time Activities</a></li>
+                        <li><a href="" >Contact Me</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
+}
+
+export default NavigationBar;
